f5dfb657164891afc2146fb5dfc6ad5c0e6cf9c3
Polqt <poyhidalgo@gmail.com>
2025-03-29 20:27:15 +0800

    chore: Update .env.example with database configuration and enhance welcome page layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,18 @@
-<h1>Hello ew</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+    <body>
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <p class="text-lg">This is a sample Laravel application.</p>
+        </div>
+    </body>
+</html>
